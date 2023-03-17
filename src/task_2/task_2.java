7739c941b293e941e0d45effbc94ade6ea337a1b
testoccgg <testoccgg@gmail.com>
2023-03-17 08:00:23 +0300

    added task_2

---
+++ src/task_2/task_2.java
@@ -0,0 +1,14 @@
+package task_2;
+
+import patterns.MVC.MVC;
+import patterns.MVP.MVP;
+import patterns.Prototype;
+
+// Реализуйте паттерны: MVC, MVP и Прототип
+public class task_2 {
+    public static void main(String[] args) {
+        MVC.main(null);
+        MVP.main(null);
+        Prototype.main(null);
+    }
+}
